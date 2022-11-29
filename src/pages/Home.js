715561d9d7f65d4dc28debf0432b48aed67c24bc
Nicolas Bogenschutz <nicolas.bogenschutz@apside-groupe.com>
2022-11-29 15:26:22 +0100

    Mise en place inavbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,38 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='section' id='Home'>
-            <p>
-            Home
-            </p>
+        <div className='section home' id='Home'>
+            <div className="parent">
+                <div className="div1">
+                    <h1> Welcome to the Nicolas Bogenschutz portfolio</h1>
+                </div>
+            <div className="div2">yo </div>
+                <div className="div3">
+                    <ul>
+                        <li><h3>Who I am ?</h3></li>
+                        <li>Nicolas Bogenschutz</li>
+                        <li>French and Brazilian</li>
+                        <li>21 years old</li>
+                        <li>Strasbourg</li>
+                    </ul>
+                </div>
+                <div className="div4">
+                <ul>
+                        <li><h3>What I do ?</h3></li>
+                        <li>Nicolas Bogenschutz</li>
+                        <li>French and Brazilian</li>
+                        <li>21 years old</li>
+                        <li>Strasbourg</li>
+                    </ul></div>
+                <div className="div5">
+                    <ul>
+                        <li><h3>What i'm currently looking for ?</h3></li>
+                        <li>Nicolas Bogenschutz</li>
+                        <li>French and Brazilian</li>
+                        <li>21 years old</li>
+                        <li>Strasbourg</li>
+                    </ul></div>
+            </div>
         </div>
     );
 };
