f21063bea2332c63c199f0450963e2816b367cd2
Nicolas BOGENSCHUTZ <nicolas.bogenschutz@viacesi.fr>
2022-11-29 21:38:49 +0100

    add home animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,12 @@ const Navbar = () => {
     return (
         <div className='Navbar'>
              <ul>
-                <li><a href='#Home'><span class="material-symbols-outlined">home</span></a></li>
-                <li><a href='#Aboutme'><span class="material-symbols-outlined">person_filled</span></a></li>
-                <li><a href='#Knowledges'><span class="material-symbols-outlined">terminal</span></a></li>
-                <li><a href='#Project'><span class="material-symbols-outlined">fact_check</span></a></li>
-                <li><a href='#Experiences'><span class="material-symbols-outlined">menu_book</span></a></li>
-                <li><a href='#Contact'><span class="material-symbols-outlined">call</span></a></li>
+                <li><a href='#Home'><span className="material-symbols-outlined">home</span></a></li>
+                <li><a href='#Aboutme'><span className="material-symbols-outlined">person_filled</span></a></li>
+                <li><a href='#Knowledges'><span className="material-symbols-outlined">terminal</span></a></li>
+                <li><a href='#Project'><span className="material-symbols-outlined">fact_check</span></a></li>
+                <li><a href='#Experiences'><span className="material-symbols-outlined">menu_book</span></a></li>
+                <li><a href='#Contact'><span className="material-symbols-outlined">call</span></a></li>
             </ul>
         </div>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,38 @@
-import React from 'react';
 
-const Home = () => {
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+
+  handleMouseMove = event => {
+    
+    const eye = document.querySelectorAll(".eyes");
+   eye.forEach(function (eye) {
+     let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+     let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+
+     let radian = Math.atan2(event.clientX - x, event.clientY - y);
+     let rotate = radian * (180 / Math.PI) * -1 + 270;
+     eye.style.transform = "rotate(" + rotate + "deg)";
+   });
+  }
+
+  render() {
     return (
         <div className='section home' id='Home'>
-            <div className="parent">
-                <div className="div1">
-                    <h1> Welcome to the Nicolas Bogenschutz portfolio</h1>
-                </div>
-            <div className="div2">yo </div>
-                <div className="div3">
+        <div className="parent" onMouseMove={this.handleMouseMove}>
+            <div className="div1">
+                <h1> Welcome to the Nicolas Bogenschutz portfolio</h1>
+            </div>
+        <div className="div2">
+        <div class="eyescontainer">
+            <div class="eyes"></div>
+            <div class="eyes"></div>
+        </div>
+        </div>
+        <div className="div3">
                     <ul>
-                        <li><h3>Who I am ?</h3></li>
+                        <h3>Who I am ?</h3>
+                        <br></br>
                         <li>Nicolas Bogenschutz</li>
                         <li>French and Brazilian</li>
                         <li>21 years old</li>
@@ -19,23 +41,25 @@ const Home = () => {
                 </div>
                 <div className="div4">
                 <ul>
-                        <li><h3>What I do ?</h3></li>
-                        <li>Nicolas Bogenschutz</li>
-                        <li>French and Brazilian</li>
-                        <li>21 years old</li>
-                        <li>Strasbourg</li>
+                        <h3>What I do ?</h3>
+                        <br></br>
+                        <li>Alternship in Computer Engineering</li>
+                        <li>Student at CESI Ecole d'ingénieurs</li>
+                        <li>IT developer apprentice</li>
                     </ul></div>
                 <div className="div5">
                     <ul>
-                        <li><h3>What i'm currently looking for ?</h3></li>
-                        <li>Nicolas Bogenschutz</li>
-                        <li>French and Brazilian</li>
-                        <li>21 years old</li>
-                        <li>Strasbourg</li>
+                        <h3>What i'm currently looking for ?</h3>
+                        <br></br>
+                        <li>An internship abroad in English country</li>
+                        <li>Improvement of my IT skills</li>
+                        <li>Improve my english</li>
                     </ul></div>
             </div>
-        </div>
-    );
-};
+    </div>
+    )
+  }
+}
 
-export default Home;
+
+
