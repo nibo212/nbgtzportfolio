d5e97e0042b84109efded107d8883b40b92f842e
Nicolas Bogenschutz <nicolas.bogenschutz@apside-groupe.com>
2022-11-30 13:58:46 +0100

    build template for about's me page

--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Aboutme = () => {
     return (
         <div className='section aboutme' id='Aboutme'>
-            Aboutme
+            <div className='parentaboutme'>
+            <div className="divCentral">gfgf </div>
+            <div className="divlefttopcorner">csc </div>
+            <div className="divleftbottomcorner"> </div>
+            <div className="divrighttopcorner"> </div>
+            <div className="divrightbottomcorner"> </div>
+            </div>
         </div>
     );
 };
