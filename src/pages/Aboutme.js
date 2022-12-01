7f09e57e0a0a1461914a7b3a09d324a6afd20c0a
Nicolas Bogenschutz <nicolas.bogenschutz@apside-groupe.com>
2022-12-01 15:16:25 +0100

    Finition de la page about me

--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -4,7 +4,9 @@ const Aboutme = () => {
     return (
         <div className='section aboutme' id='Aboutme'>
             <div className='parentaboutme'>
-                <div className="divCentral">gfgf </div>
+                <div className="divCentral">
+                    <h1>About Me</h1>
+                </div>
                 
                 <div className="divlefttopcorner">
                     <div class="container">
@@ -16,9 +18,35 @@ const Aboutme = () => {
                     </div>
                 </div>
 
-            <div className="divleftbottomcorner"> </div>
-            <div className="divrighttopcorner"> </div>
-            <div className="divrightbottomcorner"> </div>
+            <div className="divleftbottomcorner">
+            <div class="container1">
+                <div class="content1">
+                        <h1>Pratham</h1>
+                        <h3>I love designing websites and keep things as simple as possible. My goals is to focus on minimalism and conveying the message that you want to send</h3>
+                </div>
+                <div class="flap1"></div>
+            </div>
+            </div>
+                
+            <div className="divrightbottomcorner">
+                <div class="container2">
+                    <div class="content2">
+                            <h1>Pratham</h1>
+                            <h3>I love designing websites and keep things as simple as possible. My goals is to focus on minimalism and conveying the message that you want to send</h3>
+                    </div>
+                    <div class="flap2"></div>
+                </div>
+                </div>
+                
+            <div className="divrighttopcorner">
+                <div class="container3">
+                    <div class="content3">
+                            <h1>Pratham</h1>
+                            <h3>I love designing websites and keep things as simple as possible. My goals is to focus on minimalism and conveying the message that you want to send</h3>
+                    </div>
+                    <div class="flap3"></div>
+                </div>
+            </div>
             </div>
         </div>
     );
