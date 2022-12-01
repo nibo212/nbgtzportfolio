52ccd09ea64410f78d77f9c908946fe1a6d37484
Nicolas Bogenschutz <nicolas.bogenschutz@apside-groupe.com>
2022-12-01 11:58:32 +0100

    Debut de Aboutme

--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -4,8 +4,18 @@ const Aboutme = () => {
     return (
         <div className='section aboutme' id='Aboutme'>
             <div className='parentaboutme'>
-            <div className="divCentral">gfgf </div>
-            <div className="divlefttopcorner">csc </div>
+                <div className="divCentral">gfgf </div>
+                
+                <div className="divlefttopcorner">
+                    <div class="container">
+                        <div class="content">
+                            <h1>Pratham</h1>
+                            <h3>I love designing websites and keep things as simple as possible. My goals is to focus on minimalism and conveying the message that you want to send</h3>
+                        </div>
+                        <div class="flap"></div>
+                    </div>
+                </div>
+
             <div className="divleftbottomcorner"> </div>
             <div className="divrighttopcorner"> </div>
             <div className="divrightbottomcorner"> </div>
